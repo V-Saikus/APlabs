4d3f86f76da980301ed997bffe7a385b36409052
V-Saikus <wowanos88@gmail.com>
2020-11-01 22:49:38 +0200

    Completed program

--- APproject/tests/__init__.py
+++ APproject/tests/__init__.py
@@ -1,13 +1,18 @@
 from flask import Flask
-app=Flask(__name__)
+from wsgiref.simple_server import make_server
+app = Flask(__name__)
+
 
 @app.route('/')
-def home():
-    return 'Hello World'
+def hello_world():
+    return 'Hello, World!'
+
 
 @app.route('/api/v1/hello-world-23')
 def var():
     return 'Hello World 23'
 
-if __name__=='__main__':
-    app.run()
+with make_server('', 5000, app) as server:
+    print("Server srated")
+
+    server.serve_forever()
